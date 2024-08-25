76a2e29fb37c4dad55671cd2f6ada7c33425fc9c
Anna Harutyunyan <aharutyunyan745@gmail.com>
2024-08-25 19:02:15 +0400

    function that given number is a prime number or not

---
+++ isPrime.js
@@ -0,0 +1,11 @@
+function isPrime(number) {
+    for (let i = 2; i < number - 1; i++) {
+        if (number % i == 0){
+            return false
+        }
+    }
+    return true
+}
+
+const result = isPrime(5)
+console.log(result)
